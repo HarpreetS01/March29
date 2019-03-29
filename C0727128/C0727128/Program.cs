70e79422b19f9ff84c384cc5ae07b437005b74be
Harpreet Singh <727128@student.tor.loc>
2019-03-29 19:57:11 -0400

    Happy

---
+++ C0727128/C0727128/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C0727128
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HelloWorld hw = new HelloWorld();
+            hw.Hello();
+        }
+    }
+    class HelloWorld
+    {
+        public void Hello()
+        {
+            Console.WriteLine("Hello World");
+        }
+
+        public int Add(int a, int b) { return a + b; }
+    }
+}
